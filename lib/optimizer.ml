9c0bae78970bee21d83cf79813dbb14920f09c2c
0x0f0f0f <sudo-woodo3@protonmail.com>
2020-01-07 15:46:26 +0100

    propositional calculus optimizations

--- lib/optimizer.ml
+++ lib/optimizer.ml
@@ -1,38 +1,59 @@
 open Types
 
 let rec optimize (e: expr) : expr = match e with
-  | Plus(Integer x, Integer y) -> Integer (x + y) (* Autoreduce constants *)
+  (* Autoreduce constants *)
+  | Plus(Integer x, Integer y) -> Integer (x + y) 
   | Sub(Integer x, Integer y) -> Integer (x - y)
   | Mult(Integer x, Integer y) -> Integer (x * y)
   | Eq(Integer x, Integer y) -> Boolean (x == y)
   | Gt(Integer x, Integer y) -> Boolean (x > y)
   | Lt(Integer x, Integer y) -> Boolean (x < y)
-  | And(Boolean x, Boolean y) -> Boolean (x && y)
-  | Or(Boolean x, Boolean y) -> Boolean (x || y)
   | Eq(x, y) -> Eq (optimize x, optimize y)
   | Gt(x, y) -> Gt (optimize x, optimize y)
   | Lt(x, y) -> Lt (optimize x, optimize y)
-  | Not(Boolean x) -> Boolean (not x)
   | Plus(x, y) ->  Plus (optimize x, optimize y)
   | Sub(x, y) ->  Sub (optimize x, optimize y)
   | Mult(x, y) -> Mult (optimize x, optimize y)
-  | And(x, y) ->  And (optimize x, optimize y)
-  | Or(x, y) ->   Or (optimize x, optimize y)
-  | Not(x) -> Not (optimize x)
   | List(l) -> List(List.map optimize l)
   | Dict(d) -> Dict(List.map (fun (k, v) -> (optimize k, optimize v)) d)
-  | IfThenElse(guard, iftrue, iffalse) ->
-    (* Short circuit an if that is always true or false *)
-    let og = optimize guard in
-    if og = Boolean true then optimize iftrue
-    else if og = Boolean false then optimize iffalse
-    else IfThenElse(og, optimize iftrue, optimize iffalse)
   | Lambda(params, body) -> Lambda(params, optimize body)
   | Let(declarations, body) -> optimize_let declarations body false
   | Letlazy(declarations, body) -> optimize_let declarations body true
   | Letrec(ident, dec, body) -> Letrec(ident, optimize dec, optimize body)
   | Letreclazy(ident, dec, body) -> Letreclazy(ident, optimize dec, optimize body)
   | Apply(func, params) -> Apply(func, (List.map optimize params))
+  (* Propositional Calculus optimizations *)
+  | And(Boolean x, Boolean y) -> Boolean (x && y)
+  | Or(Boolean x, Boolean y) -> Boolean (x || y)
+  | Not(Not(x)) -> optimize x
+  | Not(Boolean x) -> Boolean (not x)
+  | Not(And(a, b)) -> Or(Not (optimize a), Not (optimize b))    (* DeMorgan *)
+  | Not(Or(a, b)) -> And(Not (optimize a), Not (optimize b))    
+  | Or(a, And (Not aa, b)) ->           (* Complement *)
+    let oa = optimize a and oaa = optimize aa and ob = optimize b in
+    if oa = oaa then Or(oa, ob)
+    else Or(oa, And (Not oaa, ob))
+  | And(a, Or (Not aa, b)) -> 
+    let oa = optimize a and oaa = optimize aa and ob = optimize b in
+    if oa = oaa then And(oa, ob)
+    else And(oa, Or (Not oaa, ob))
+  | Or(a, And (aa, b)) ->               (* Absorb *)
+    let oa = optimize a and oaa = optimize aa and ob = optimize b in
+    if oa = oaa then oa
+    else Or(oa, And (oaa, ob))
+  | And(a, Or (aa, b)) ->               (* Absorb *)
+    let oa = optimize a and oaa = optimize aa and ob = optimize b in
+    if oa = oaa then oa
+    else And(oa, Or (oaa, ob))
+  | Not(x) -> Not (optimize x)
+  | And(x, y) ->  And (optimize x, optimize y)
+  | Or(x, y) ->   Or (optimize x, optimize y)
+  | IfThenElse(guard, iftrue, iffalse) ->
+    (* Short circuit an if that is always true or false *)
+    let og = optimize guard in
+    if og = Boolean true then optimize iftrue
+    else if og = Boolean false then optimize iffalse
+    else IfThenElse(og, optimize iftrue, optimize iffalse)
   | _ -> e
 and optimize_let declarations body islazy =
   let od = List.map (fun (i, v) -> (i, optimize v)) declarations in
