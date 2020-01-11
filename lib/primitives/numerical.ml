815352bcc72671d0c32d0ac0130e2c336f2b8c77
0x0f0f0f <sudo-woodo3@protonmail.com>
2020-01-11 20:50:50 +0100

    integer division returns a float if the modulo is 0

--- lib/primitives/numerical.ml
+++ lib/primitives/numerical.ml
@@ -53,7 +53,10 @@ let div args =
     | [x; y] -> (x, y)
     | _ -> raise (WrongPrimitiveArgs)) in
   match found with
-  | TInt -> EvtInt (unpack_int x / unpack_int y)
+  | TInt -> (* TOWRITE *)
+    let xx = unpack_int x and yy = unpack_int y in
+    if xx mod yy = 0 then EvtInt (xx / yy)
+    else EvtFloat (float_of_int (unpack_int x) /. float_of_int (unpack_int y))
   | TFloat -> EvtFloat (unpack_float x /. unpack_float y)
   | TComplex -> EvtComplex (Complex.div (unpack_complex x) (unpack_complex y))
   | _ -> raise (TypeError "not a number")
